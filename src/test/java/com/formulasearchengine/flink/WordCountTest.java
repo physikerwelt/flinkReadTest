a3906254947ba2e923f0c51254a64d5874597552
Moritz Schubotz <wiki@physikerwelt.de>
2017-05-21 09:08:11 +0200

    Add automated test

---
+++ src/test/java/com/formulasearchengine/flink/WordCountTest.java
@@ -0,0 +1,11 @@
+package com.formulasearchengine.flink;
+
+/**
+ * Created by Moritz on 20.05.2017.
+ */
+public class WordCountTest {
+    @org.junit.Test
+    public void main() throws Exception {
+        WordCount.main(null);
+    }
+}
